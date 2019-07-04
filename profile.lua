c13abd0baa668e03cbf27714e8b7529ba50add97
Matthew Fallshaw <m@fallshaw.me>
2019-07-04 12:22:35 -0700

    table extensions profile cleanup

--- profile.lua
+++ profile.lua
@@ -1,15 +1,30 @@
-local function functionWithTimes(fn, ...)
-  logger.w(hs.timer.secondsSinceEpoch())
+local M = {}
+
+local function currentTime()
+  return tonumber(
+    (
+      hs.execute("/usr/local/opt/coreutils/libexec/gnubin/date +%s%N")
+    )
+  )/1000000000
+end
+M.currentTime = currentTime
+
+function M.functionWithTimes(fn, ...)
+  local start = current_time()
   result = table.pack(fn(...))
-  logger.w(hs.timer.secondsSinceEpoch())
+  print("elapsed seconds: ".. tostring(current_time() - start))
   return table.unpack(result)
 end
-local function profilingOn()
+
+function M.profilingOn()
   debug.sethook(function (event)
     local x = debug.getinfo(2, 'nS')
     print(event, x.name, x.linedefined, x.source, hs.timer.secondsSinceEpoch())
   end, "c")
 end
-local function profilingOff()
+
+function M.profilingOff()
   debug.sethook()
 end
+
+return M
--- table.lua
+++ table.lua
@@ -24,3 +24,47 @@ local function iteratorToArray(...)
   end
   return arr
 end
+
+-- or use `tbl = Table(...)` as a constructor then use t:keys() etc.
+local mt = { __index = table }
+function Table(t)
+  return setmetatable(t or {}, mt)
+end
+
+table.keys = function(self)
+  local keys,i = {},0
+  for k,_ in pairs(self) do
+    i = i + 1
+    keys[i] = k
+  end
+  return keys
+end
+
+table.values = function(self)
+  local values,i = {},0
+  for _,v in pairs(self) do
+    i = i + 1
+    values[i] = v
+  end
+  return values
+end
+
+table.clone = function(self)
+  local out = {}
+  for k,v in pairs(self) do
+    if type(v) == 'table' then
+      out[k] = table.clone(v)
+    else
+      out[k] = v
+    end
+  end
+  return out
+end
+
+table.head = function(self)
+  return self[1]
+end
+
+table.tail = function(self)
+  return { select(2, table.unpack(self)) }
+end
